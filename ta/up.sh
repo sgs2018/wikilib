1038028e77c1198bae0aaedd737d07ad971fd6fd
sgsName <sgs@163.com>
2019-08-06 08:37:54 +0800

    dirtxt

---
+++ ta/up.sh
@@ -0,0 +1,11 @@
+#mydata github.dl.dir.ta cmdunix 20195
+#dl.dir to github
+
+
+
+git clone git@github.com:sgs2018/wikilib.git
+ 
+cp /sdcard/download/t/*  ta
+git add ta/*
+git commit -m dirtxt
+git push origin master
